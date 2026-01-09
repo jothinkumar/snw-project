156151ee0e3082b047abf1d7bbace0b55cc0c73d
JOTHINKUMAR B <nst@joth.in>
2026-01-09 16:35:36 +0530

    options on hover in navbar

---
+++ src/components/NavBar/script.js
@@ -0,0 +1,10 @@
+Array.from(document.getElementsByClassName('navbar-opt')).forEach(element => {
+    const hoverWindowId=element.id+"-hoverwindow"
+    const hoverWindow=document.getElementById(hoverWindowId)
+    const parentId=element.id+"-parent"
+    const parent=document.getElementById(parentId)
+    const show =()=>hoverWindow.style.display="block"
+    const hide =()=>hoverWindow.style.display="none"
+    element.addEventListener("mouseover",show)
+    parent.addEventListener("mouseleave",hide)
+});
